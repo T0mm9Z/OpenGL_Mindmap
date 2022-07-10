faf736b698e9f97afe91ec6590dbc75b63db0f3c
T0mm9Z <52064916+T0mm9Z@users.noreply.github.com>
2022-07-10 17:16:15 +0200

    Add files via upload

---
+++ OpenGL.mm
@@ -0,0 +1,101 @@
+<map version="freeplane 1.7.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="OpenGL" FOLDED="false" ID="ID_124711911" CREATED="1657383284885" MODIFIED="1657385003825" STYLE="oval">
+<font SIZE="18"/>
+<hook NAME="MapStyle">
+    <properties edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" fit_to_viewport="false"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ICON_SIZE="12.0 pt" COLOR="#000000" STYLE="fork">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10.0 pt" SHAPE_VERTICAL_MARGIN="10.0 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="5" RULE="ON_BRANCH_CREATION"/>
+<node TEXT="Helpers" POSITION="right" ID="ID_1457431636" CREATED="1657383468797" MODIFIED="1657383471908">
+<edge COLOR="#00ff00"/>
+<node TEXT="GLEW" ID="ID_1994933355" CREATED="1657383293018" MODIFIED="1657384002745"/>
+<node TEXT="GLFW" ID="ID_140063247" CREATED="1657383297738" MODIFIED="1657383995637">
+<node TEXT="Helper for" ID="ID_352078932" CREATED="1657383549440" MODIFIED="1657383559049">
+<node TEXT="Vulkan" ID="ID_1681647230" CREATED="1657383529255" MODIFIED="1657383531805"/>
+<node TEXT="OpenGLES" ID="ID_194041984" CREATED="1657383515431" MODIFIED="1657383522605"/>
+<node TEXT="OpenGL" ID="ID_968551470" CREATED="1657383497542" MODIFIED="1657383507430"/>
+</node>
+<node TEXT="Supports" ID="ID_1822935729" CREATED="1657383649387" MODIFIED="1657383652595">
+<node TEXT="Windows" ID="ID_362016128" CREATED="1657383653427" MODIFIED="1657383655793"/>
+<node TEXT="MacOS" ID="ID_376597103" CREATED="1657383656291" MODIFIED="1657383658433"/>
+<node TEXT="X11" ID="ID_896904094" CREATED="1657383658819" MODIFIED="1657383661252"/>
+<node TEXT="Wayland" ID="ID_371351078" CREATED="1657383662059" MODIFIED="1657383664290"/>
+</node>
+</node>
+<node TEXT="GLUT3" ID="ID_670610886" CREATED="1657384909797" MODIFIED="1657384913786"/>
+</node>
+<node TEXT="Abbreviations" POSITION="left" ID="ID_911119300" CREATED="1657383974344" MODIFIED="1657383977588">
+<edge COLOR="#ff00ff"/>
+<node TEXT="GLEW (Graphics Library Extension Wrangler)" ID="ID_1807655785" CREATED="1657383984078" MODIFIED="1657383985549"/>
+<node TEXT="GLFW (GraphicS Library Framework)" ID="ID_1713237976" CREATED="1657384005355" MODIFIED="1657384006585"/>
+<node TEXT="GLEE (Graphics Library Easy Extensions)" ID="ID_415754532" CREATED="1657384310525" MODIFIED="1657384332587"/>
+<node TEXT="GLUT (Graphics Library Utility Toolkit Lib)" ID="ID_1527372604" CREATED="1657384920993" MODIFIED="1657389569141"/>
+<node TEXT="GLUT (Graphics Library Utility Lib)" ID="ID_35785535" CREATED="1657389549591" MODIFIED="1657389572625"/>
+<node TEXT="DRM(Direct Rendering Manager)" ID="ID_916276837" CREATED="1657465491307" MODIFIED="1657465526124"/>
+<node TEXT="KMS(Kernel Mode Settings)" ID="ID_1785146367" CREATED="1657465541384" MODIFIED="1657465550970"/>
+<node TEXT="SCI(System Call Interface)" ID="ID_164406410" CREATED="1657465595507" MODIFIED="1657465603499"/>
+</node>
+<node TEXT="" POSITION="right" ID="ID_402729929" CREATED="1657384200616" MODIFIED="1657385003824" HGAP_QUANTITY="68.7499983683229 pt" VSHIFT_QUANTITY="115.49999655783186 pt">
+<edge COLOR="#00ffff"/>
+<hook URI="OpenGL_files/png_8846286798062968729.png" SIZE="0.35087502" NAME="ExternalObject"/>
+</node>
+</node>
+</map>
